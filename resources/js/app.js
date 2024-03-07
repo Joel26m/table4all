9d9d53bc6177b60a06f7b3972515311dfa268397
mplanse <alexventuravilalta04@gmail.com>
2024-03-07 19:19:15 +0100

    new

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,55 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
 
-import * as bootstrap from 'bootstrap'
+mapboxgl.accessToken = 'pk.eyJ1IjoidmVudHUwMCIsImEiOiJjbHN3MzY5cTkwbWU4MmttdHg2NnhvaDV2In0.4i_tTPy63h2OHahnuJsQpw';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/ventu00/clsw58v7j007301qjfa3of8ut',
+  center: [2.172850, 41.389280],
+  zoom: 10.7
+});
+map.on('click', (event) => {
+  const popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML(
+      `<h3>${feature.properties.nombre}</h3><p>Dirección: ${feature.properties.direccion}</p>`
+    )
+    .addTo(map);
+});
+
+const proveedorMarker = new mapboxgl.Marker({ element: createCustomMarker() })
+  .setLngLat([2.1734, 41.3851])
+  .setPopup(new mapboxgl.Popup().setHTML("<h3>Proveedor</h3>"))
+  .addTo(map);
+
+const markerElement = proveedorMarker.getElement();
+
+markerElement.classList.add('proveedor-marker');
+
+function createCustomMarker() {
+  const el = document.createElement('div');
+  el.className = 'proveedor-marker';
+  return el;
+}
+
+
+
+
+
+
+const beneficiaryMarker = new mapboxgl.Marker({ element: createCustomMarkerb() })
+  .setLngLat([2.0330500, 41.4922600])
+  .setPopup(new mapboxgl.Popup().setHTML("<h3>Beneficiario</h3>"))
+  .addTo(map);
+
+// Obtener el elemento DOM asociado al marcador
+const markerElementb = beneficiaryMarker.getElement();
+
+// Agregar la clase "proveedor-marker" al elemento DOM del marcador
+markerElementb.classList.add('beneciciary-marker');
+
+function createCustomMarkerb() {
+  const el = document.createElement('div');
+  el.className = 'beneciciary-marker';
+  return el;
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,39 +5,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @vite(['resources/css/app.css', 'resources/css/app.scss', 'resources/js/app.js'])
+
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
 </head>
 <body>
+
+
 <nav class="navbar navbar-expand-lg bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
+    <a class="logo navbar-brand" href="#"></a>
+    <!-- Botón para abrir el offcanvas -->
+    <!-- Contenido de la barra de navegación -->
+    <div class="desplegable collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ml-auto">
+
     </div>
+    <button class="custom-toggler bg-primary navbar-toggler d-block" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="bg-primary navbar-toggler-icon custom-white"></span>
+    </button>
   </div>
+  
 </nav>
 
+<!-- Offcanvas -->
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Navbar</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <!-- Contenido del navbar -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown link
+        </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<div id='map'></div>
 </body>
 </html>
