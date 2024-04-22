2c3d6a9bc3c3d9d26852b6b1ce644685108a256a
marc <mplanse4@gmail.com>
2024-04-22 17:32:01 +0200

    new

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,45 @@ import nav from './components/nav.vue'
 
 createApp(nav).mount('#nav')
 
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
 
+  // Función para obtener el valor de una cookie
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var btnDesaparecer = document.getElementById("btnDesaparecer");
+    btnDesaparecer.addEventListener("click", function() {
+      var divGenially = document.querySelector(".container-wrapper-genially");
+      divGenially.style.display = "none";
+      btnDesaparecer.style.display = "none"; // Oculta el botón también
+      setCookie("visited", "true", 30); // Establece la cookie 'visited' con valor 'true' durante 30 días
+    });
+
+    var visited = getCookie("visited");
+    if (visited) {
+      var divGenially = document.querySelector(".container-wrapper-genially");
+      var btnDesaparecer = document.getElementById("btnDesaparecer");
+      divGenially.style.display = "none";
+      btnDesaparecer.style.display = "none"; // Oculta el botón si se ha visitado antes
+    }
+  });
 (function(d) {
     var js, id = "genially-embed-js",
         ref = d.getElementsByTagName("script")[0];
--- resources/views/nav/index.blade.php
+++ resources/views/nav/index.blade.php
@@ -20,12 +20,19 @@
 <body>
 <div id="nav"></div> <!-- es un compoenente -->
 
+<div class="usarappbutton">
+
+</div>
+
 <div class="container-wrapper-genially" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 0;">
-    <video class="loader-genially" autoplay loop playsinline muted style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 80px; height: 80px;">
+<button id="btnDesaparecer" class="btn btn-primary">¡Ya sé usar Bitiby!</button>
+   
+<video class="loader-genially" autoplay loop playsinline muted style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 80px; height: 80px;">
         <source src="https://static.genial.ly/resources/loader-default.mp4" type="video/mp4" />
         Your browser does not support the video tag.
     </video>
     <div id="662281eb57c6e800147b41d7" class="genially-embed" style="margin: 0 auto; position: relative; width: 100%; height: 100%;"></div>
+
 </div>
 
 
