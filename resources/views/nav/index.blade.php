d2c0506b56cd3858e699476bfbd6e8b6b896b364
Ventu00 <79811635+Ventu00@users.noreply.github.com>
2024-05-04 18:14:21 +0200

    new

--- resources/views/nav/index.blade.php
+++ resources/views/nav/index.blade.php
@@ -127,6 +127,8 @@
     </div>
   </div>
 </div>
+<div style="width: 100%;"><div style="position: relative; padding-bottom: 177.78%; padding-top: 0; height: 0;"><iframe title="Presentación interactiva básica móvil" frameborder="0" width="1080" height="1920" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/662281eb57c6e800147b41d7" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe> </div> </div>
+
 
 <!-- Chatbot -->
 <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
